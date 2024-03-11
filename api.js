7d8c5d27b5c5bede783ab3ae60da9f671345b048
samm021 <ismail.pribadi@xendit.co>
2024-03-11 18:12:43 +0700

    feat: cli app

---
+++ api.js
@@ -0,0 +1,96 @@
+const baseUrl = require('./config').BASE_URL;
+const issueUrl = `${baseUrl}/api/issues`;
+
+const baseOptions = {
+  headers: {
+    Accept: 'application/json',
+    origin: 'https://localhost:3001',
+    "Content-Type": 'application/json;charset=UTF-8',
+  },
+}
+
+const createIssue = async (title, description) => {
+  try {
+    const options = {
+      ...baseOptions,
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        description
+      })
+    };
+
+    const issue = await fetch(issueUrl, options);
+
+    return issue.json();
+  } catch (e) {
+    console.error(e.message);
+  }
+}
+
+const getIssues = async () => {
+  try {
+    const options = {
+      ...baseOptions,
+      method: 'GET'
+    };
+    const issues = await fetch(issueUrl, options);
+    return issues.json();
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+const getIssueById = async (id) => {
+  try {
+    const options = {
+      ...baseOptions,
+      method: 'GET'
+    };
+    const issue = await fetch(`${issueUrl}/${id}`, options);
+    return issue.json();
+  } catch (e) {
+    console.error(e.message);
+  }
+}
+
+const updateIssue = async (id, title, description) => {
+  try {
+    const body = {};
+    if (title) body.title = title;
+    if (description) body.description = description;
+
+    const options = {
+      ...baseOptions,
+      method: 'PATCH',
+      body: JSON.stringify(body)
+    };
+
+    const issue = await fetch(`${issueUrl}/${id}`, options);
+    return issue.json();
+  } catch (e) {
+    console.error(e.message);
+  }
+}
+
+const deleteIssue = async (id) => {
+  try {
+    const options = {
+      ...baseOptions,
+      method: 'DELETE',
+    };
+
+    await fetch(`${issueUrl}/${id}`, options);
+    return getIssues();
+  } catch (e) {
+    console.error(e.message);
+  }
+}
+
+module.exports = {
+  createIssue,
+  getIssues,
+  getIssueById,
+  updateIssue,
+  deleteIssue
+}
---
+++ index.js
@@ -0,0 +1,82 @@
+const readlineSync = require('readline-sync');
+const api = require('./api');
+
+function initInstructions() {
+  console.info('save / 1        : create/save issue');
+  console.info('read all / 2    : read all saved issues');
+  console.info('read / 3        : read issue (by issue id)');
+  console.info('update / 4      : update issue (by issue id)');
+  console.info('delete / 5      : delete an issue (by issue id)');
+  console.info('--help          : display instructions');
+  console.info('--exit          : exit the program');
+}
+
+async function initList() {
+  const choice = readlineSync.question('Enter input: ');
+
+  switch (choice) {
+    case '1':
+    case 'save':
+      const titleToSave = readlineSync.question('Enter issue title: ');
+      const descriptionToSave = readlineSync.question('Enter issue description: ');
+      const savedIssue = await api.createIssue(titleToSave, descriptionToSave);
+  
+      console.info('Issue added successfully.');
+      console.info(savedIssue);
+      break;
+    
+    case '2':
+    case 'read all':
+      const issues = await api.getIssues();
+      issues.forEach((issue, idx) => console.log(`no. ${idx + 1}`, issue));
+      break;
+
+    case '3':
+    case 'read':
+      const idToRead = readlineSync.question('Enter issue ID to read:  ');;
+      const fetchedIssue = await api.getIssueById(idToRead);
+      console.info(fetchedIssue);
+      break;
+
+    case '4':
+    case 'update':
+      const idToUpdate = readlineSync.question('Enter issue ID to update:  ');
+      const issueToUpdate = await api.getIssueById(idToUpdate);
+      console.info(issueToUpdate);
+      const titleToUpdate = readlineSync.question('Enter issue title (press enter to skip):  ');
+      const descriptionToUpdate = readlineSync.question('Enter issue (press enter to skip):  ');
+      const updatedIssue = await api.updateIssue(idToUpdate, titleToUpdate, descriptionToUpdate);
+
+      console.info(`Issue ${idToUpdate} updated successfully.`);
+      console.info(updatedIssue);
+      break;
+
+    case '5':
+    case 'delete':
+      const idToDelete = readlineSync.question('Enter issue ID to delete:  ');
+      await api.deleteIssue(idToDelete);
+
+      console.info(`Issue ${idToDelete} deleted successfully.`);
+      break;
+    
+    case '--help':
+      initInstructions();
+      break;
+
+    case '--exit':
+      console.info('Exiting the app.');
+      process.exit();
+
+    default:
+      console.info('Input invalid. Input --help to see available options');
+  }
+
+  initInstructions();
+  initList();
+}
+
+
+// Start the app
+console.log('Welcome to Issue CLI app!');
+initInstructions();
+initList();
